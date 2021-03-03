30d67003d5bfee650606b6f38bdd328cc2d34d0b
tkhhhh <45356508+tkhhhh@users.noreply.github.com>
2021-03-03 16:25:31 +0800

    Add files via upload

--- 221801313/src/WordCount.py
+++ 221801313/src/WordCount.py
@@ -5,19 +5,34 @@ import sys
 class Main(object):
     def DealMethod(self,input,output):
         CharDeals = CharDeal()
-        FIn = open(input,encoding='utf-8')
-        strling = FIn.read()
-        lines = CharDeals.LineCount(strling)
-        characters = CharDeals.CharCount(strling)
-        total_num, new_wordlist, word_num = CharDeals.WordsCount(characters,strling)
-        FOut = open(output,encoding='utf-8')
-        FOut.write("characters:" + characters + "\n")
-        FOut.write("words:" + total_num + "\n")
-        FOut.write("lines:" + lines + "\n")
-        for i in range(len(word_num)):
-            FOut.write(new_wordlist[i] + word_num[i] + "\n")
-        FIn.close()
-        FOut.close()
+        try:
+            FIn = open(input,'r',encoding='utf-8')
+            strling = FIn.read()
+            lines = CharDeals.LineCount(strling)
+            characters = CharDeals.CharCount(strling)
+            total_num, new_wordlist, word_num = CharDeals.WordsCount(characters,strling)
+        except IOError:
+            print("input file open error")
+        except ValueError as e:
+            print("value error,",e)
+        except TypeError as e:
+            print("type error",e)
+        else:
+            try:
+                FOut = open(output,'w',encoding='utf-8')
+            except IOError:
+                print("output file open error")
+            else:
+                FOut.write("characters : " + str(characters) + "\n")
+                FOut.write("words : " + str(total_num) + "\n")
+                FOut.write("lines : " + str(lines) + "\n")
+                for i in range(len(word_num)):
+                    FOut.write(new_wordlist[i] + " : " + str(word_num[i]) + "\n")
+                try:
+                    FIn.close()
+                    FOut.close()
+                except IOError:
+                    print("file close error")
 
 if __name__ == "__main__":
     input = sys.argv[1]
